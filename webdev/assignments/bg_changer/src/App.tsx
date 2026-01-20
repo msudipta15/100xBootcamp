037798264fcd5d13f2ee5a07d454865685027544
Sudipta <techtv70@gmail.com>
2026-01-20 19:16:03 +0530

    .

--- webdev/assignments/bg_changer/src/App.tsx
+++ webdev/assignments/bg_changer/src/App.tsx
@@ -1,25 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [bg, setbg] = useState("bg-white");
   return (
-    <>
-      <div className="w-full fixed bottom-0 left-0 text-white">
-        <div className="flex w-full justify-center gap-10 mb-10">
-          <button className="py-2 px-5 rounded-md border bg-red-500">
-            red
+    <div className={`w-full min-h-screen ${bg}`}>
+      <div className="w-full fixed bottom-0 left-0 text-white ">
+        <div className="flex w-full justify-center gap-10 p-10 ">
+          <button
+            onClick={() => setbg("bg-red-500")}
+            className="py-2 px-5 rounded-md  bg-red-500 cursor-pointer "
+          >
+            Red
           </button>
-          <button className="py-2 px-5 rounded-md border bg-yellow-300">
-            yellow
+          <button
+            onClick={() => setbg("bg-yellow-300")}
+            className="py-2 px-5 rounded-md  bg-yellow-300 cursor-pointer"
+          >
+            Yellow
           </button>
-          <button className="py-2 px-5 rounded-md border bg-blue-500">
-            blue
+          <button
+            onClick={() => setbg("bg-blue-500")}
+            className="py-2 px-5 rounded-md  bg-blue-500 cursor-pointer"
+          >
+            Blue
           </button>
-          <button className="py-2 px-5 rounded-md border bg-green-600">
-            green
+          <button
+            onClick={() => setbg("bg-green-500")}
+            className="py-2 px-5 rounded-md  bg-green-600 cursor-pointer"
+          >
+            Green
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
