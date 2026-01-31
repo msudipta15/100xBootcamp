9ce2ce5467851dea87ff204d854b814145edc018
Sudipta <techtv70@gmail.com>
2026-01-31 19:05:38 +0530

    .

--- webdev/assignments/hotelbooking_backend/dist/app.js
+++ webdev/assignments/hotelbooking_backend/dist/app.js
@@ -1,4 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hello");
+import express, { Router } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(Router());
+async function main() {
+    try {
+        console.log("Connecting to db....");
+        if (process.env.db_url) {
+            await mongoose.connect(process.env.db_url);
+            console.log("Connected");
+        }
+        else {
+            console.log("Invalid db url !");
+        }
+    }
+    catch (error) {
+        console.log("Something went wrong in db connection !");
+    }
+}
+main();
 //# sourceMappingURL=app.js.map
--- webdev/assignments/hotelbooking_backend/src/app.ts
+++ webdev/assignments/hotelbooking_backend/src/app.ts
@@ -1,1 +1,25 @@
-console.log("hello");
+import express, { Router } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(Router());
+
+async function main() {
+  try {
+    console.log("Connecting to db....");
+    if (process.env.db_url) {
+      await mongoose.connect(process.env.db_url);
+      console.log("Connected");
+    } else {
+      console.log("Invalid db url !");
+    }
+  } catch (error) {
+    console.log("Something went wrong in db connection !");
+  }
+}
+
+main();
