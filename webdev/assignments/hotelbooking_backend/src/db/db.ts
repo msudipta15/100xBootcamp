2d93461e4b1878b1d7534da4254a2395354da138
Sudipta <techtv70@gmail.com>
2026-01-31 20:00:00 +0530

    .

--- webdev/assignments/hotelbooking_backend/src/db/db.ts
+++ webdev/assignments/hotelbooking_backend/src/db/db.ts
@@ -1,19 +1,19 @@
 import mongoose, { Schema, set } from "mongoose";
 
 const userSchema = new Schema({
-  name: { type: String, require: true },
-  email: { type: String, unique: true, require: true },
-  password: { type: String, require: true },
-  role: { enum: ["owner", "customer"], require: true },
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { enum: ["owner", "customer"], required: true },
   phone: { type: Number },
-  created_at: { type: Date, default: Date.now() },
+  created_at: { type: Date, default: Date.now },
 });
 
 const hotelSchema = new Schema({
-  name: { type: String, require: true },
-  owner_id: { type: mongoose.Types.ObjectId, ref: "user" },
-  description: { type: String, require: true },
-  city: { type: String, require: true },
+  name: { type: String, required: true },
+  owner_id: { type: mongoose.Types.ObjectId, ref: "users" },
+  description: { type: String, required: true },
+  city: { type: String, required: true },
   amenities: { type: [], default: [] },
   rating: {
     type: Number,
@@ -23,5 +23,24 @@ const hotelSchema = new Schema({
     set: (v: number) => Math.round(v * 10) / 10,
   },
   total_reviews: { type: Number, default: 0 },
-  created_at: { type: Date, default: Date.now() },
+  created_at: { type: Date, default: Date.now },
 });
+
+const roomSchema = new Schema({
+  hotel_id: { type: mongoose.Types.ObjectId, ref: "hotels" },
+  room_number: { type: String, required: true },
+  room_type: { type: String, required: true },
+  max_occupancy: { type: Number, required: true },
+  price_per_night: { type: Number, required: true },
+  created_at: { type: Date, default: Date.now },
+});
+
+// This is done to make the hotel_id and room_number unique
+// No hotel can have 2 rooms with same room numbers
+roomSchema.index(
+  {
+    hotel_id: 1,
+    room_number: 1,
+  },
+  { unique: true },
+);
